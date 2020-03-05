4d3a3342da9bc86d5714675ee8fcdcbd9da14fb1
Tejaswini <tejaswinikumar94@gmail.com>
2020-03-04 19:21:05 -0800

    responsive updates

--- main.js
+++ main.js
@@ -14,3 +14,44 @@
 //     }
 //   );
 //   wow.init();
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+
+    var dataText = [ "based in Vacouver", "passionate about new ideas and new ways for problem solving", "fascinated by tools for thought and creation", "a dog lover", "a fitness enthusiast", "a fashion freak", "curious about Design"];
+    
+  
+    function typeWriter(text, i, fnCallback) {
+  
+      if (i < (text.length)) {
+       document.querySelector(".typewrite").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+  
+        setTimeout(function() {
+          typeWriter(text, i + 1, fnCallback)
+        }, 100);
+      }
+      
+      else if (typeof fnCallback == 'function') {
+       
+        setTimeout(fnCallback, 600);
+      }
+    }
+   
+     function StartTextAnimation(i) {
+       if (typeof dataText[i] == 'undefined'){
+          setTimeout(function() {
+            StartTextAnimation(0);
+          }, 20000);
+       }
+     
+      if (i < dataText[i].length) {
+      
+       typeWriter(dataText[i], 0, function(){
+       
+         StartTextAnimation(i + 1);
+       });
+      }
+    }
+  
+    StartTextAnimation(0);
+  });
